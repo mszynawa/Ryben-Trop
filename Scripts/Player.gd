8dd2a26b8a3c711a23c3f71a1fd5c11ede9d04b5
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2019-02-19 21:50:32 +0100

    Add files via upload

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody
 
-var speed = 25
+export var speed = 25
 var direction = Vector3(0, 0, 0)
-var velocity = Vector3()
+var collision = Vector3()
 
 #onready var leftScreen = utils.get_main_node().get_node("LeftScreen")
 #onready var rightScreen = utils.get_main_node().get_node("RightScreen")
@@ -10,6 +10,7 @@ signal playerDead
 
 func _ready():
 	#leftScreen.connect("gui_input", self, "change_position")
+	add_to_group("player")
 	pass
 	
 func _physics_process(delta):
@@ -26,22 +27,24 @@ func _physics_process(delta):
 	direction = direction.normalized()
 	direction = direction * speed * delta
 	
-	velocity = move_and_collide(direction)
-	#print(velocity)
-	if velocity:
-		#queue_free()
-		emit_signal("playerDead")
-	#print(get_transform())
+	collision = move_and_collide(direction)
+	
+	if collision:
+		#print(collision.collider)	
+		if not collision.collider.get_parent().is_in_group("obstacles"):
+			emit_signal("playerDead")
+		#elif collision.collider.get_parent().is_in_group("obstacles"):
+		#	collision.collider.get_parent().queue_free()
 	pass
 
 func _on_LeftScreen_gui_input(event):
 	#if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 	if event is InputEventScreenTouch and event.pressed:
 	
-		print(event)
+		#print(event)
 		direction = Vector3(0, 0, 0)
 		direction.z -= 1
-		velocity = move_and_collide(direction)
+		collision = move_and_collide(direction)
 	pass
 
 func _on_RightScreen_gui_input(event):
@@ -49,5 +52,5 @@ func _on_RightScreen_gui_input(event):
 	if event is InputEventScreenTouch and event.pressed:
 		direction = Vector3(0, 0, 0)
 		direction.z += 1
-		velocity = move_and_collide(direction)
+		collision = move_and_collide(direction)
 	pass
