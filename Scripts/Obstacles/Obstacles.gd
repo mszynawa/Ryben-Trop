f66b9dd40781b4aa631eaacdb90b39feeab68194
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2019-02-20 19:51:03 +0100

    Add files via upload
    Dodanie timera do power upa.

--- Scripts/Obstacles/Obstacles.gd
+++ Scripts/Obstacles/Obstacles.gd
@@ -2,5 +2,5 @@ extends MeshInstance
 
 func _ready():
 	add_to_group("obstacles")
-	get_node("Area").connect("body_entered", self, "on_body_entered")
+	get_node("Area").connect("body_entered", self, "on_body_entered")	
 	pass
--- Scripts/Obstacles/PoweUp.gd
+++ Scripts/Obstacles/PoweUp.gd
@@ -6,4 +6,17 @@ func on_body_entered(body):
 		var speed = body.speed
 		body.speed = speed + (speed * 30 / 100)
 		#print(body.speed)
+		
+	
+		var timer = Timer.new()
+		timer.connect("timeout", self, "boost_over")
+		timer.wait_time = 5
+		body.add_child(timer) #to process
+		timer.start() #to start
+	pass
+	
+func boost_over():
+	var player = utils.get_main_node().get_node("Player")
+	var speed = player.speed
+	player.speed = speed - (speed * 30 / 100)
 	pass
