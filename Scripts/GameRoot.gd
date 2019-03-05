83aaf59f7f321ec94b9a0a5262b18d7cf1d852af
mszynawa <m.szynawa@yahoo.com>
2019-03-05 11:02:33 +0100

    Usunięcie starych obiektów ścian, dodanie timera do GameRoot.

--- Scripts/GameRoot.gd
+++ Scripts/GameRoot.gd
@@ -1,9 +1,21 @@
 extends Spatial
 
+var timer = null
+
+signal addScore
+
 func _ready():
 	get_tree().paused = false
 	var player = utils.get_main_node().get_node("Fish")
 	player.connect("playerDead", self, "onPlayerDead")
+	
+	#Timer
+	timer = Timer.new()
+	add_child(timer)
+	timer.connect("timeout", self, "onTimerTimeout")
+	timer.set_wait_time(1.0)
+	timer.set_one_shot(true)
+	timer.start()
 	pass
 
 func onPlayerDead():
@@ -11,4 +23,8 @@ func onPlayerDead():
 	
 	var gameover = preload("res://Scenes/HUD/ContainerGameover.tscn").instance()
 	get_node("hud").add_child(gameover)
+	pass
+
+func onTimerTimeout():
+	
 	pass
