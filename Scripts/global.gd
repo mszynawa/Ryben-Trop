abd828306cac05c79880e5c98f782cec837f3497
Maciej <kosmadi@gmail.com>
2019-03-05 21:48:00 +0100

    no message

---
+++ Scripts/global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var playerName = "Player 1"
+
+var playerScore = 0
+
+func getPlayerName():
+	return playerName
+
+func getPlayerScore():
+	return playerScore
+
+func setPlayerScore(score):
+	playerScore = playerScore + score
+	pass
