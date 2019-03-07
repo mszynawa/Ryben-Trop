2f709ceca424d498c2ad11aa5531afb5feee6a4b
mszynawa <m.szynawa@yahoo.com>
2019-03-07 18:28:15 +0100

    Finalny commit z poprawionym GameOver i MainMenu (oraz Credits, Scoreboard).

--- Scripts/GUI/MainMenu.gd
+++ Scripts/GUI/MainMenu.gd
@@ -8,5 +8,15 @@ func _ready():
 
 func _on_TextureButton_gui_input(event):
 	if event is InputEventScreenTouch and event.pressed:
+#warning-ignore:return_value_discarded
 		get_tree().change_scene("res://Scenes/GameRoot.tscn")
-	pass
+
+func _on_ScoreButton_gui_input(event):
+	if event is InputEventScreenTouch and event.pressed:
+#warning-ignore:return_value_discarded
+		get_tree().change_scene("res://Scenes/Menu/Scoreboard.tscn")
+
+func _on_CreditsButton_gui_input(event):
+	if event is InputEventScreenTouch and event.pressed:
+#warning-ignore:return_value_discarded
+		get_tree().change_scene("res://Scenes/Menu/Credits.tscn")
