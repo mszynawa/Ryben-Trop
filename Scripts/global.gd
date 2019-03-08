c9e3353dda76b9aae180e84eeec6762d9e56328d
mszynawa <m.szynawa@yahoo.com>
2019-03-08 09:28:01 +0100

    Scena globalna pod dźwięki.

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var playerName = "Player 1"
+var playerName = "Player1"
 
 var playerScore = 0
 
@@ -16,33 +16,4 @@ func setPlayerScore(score):
 	
 func restartPlayerScore():
 	playerScore = 0
-	pass
-
-# SCOREBOARD
-var bestscore = 0 setget set_bestscore
-const filepath = "user:scoreboard.data"
-
-func _ready():
-	load_scoreboard()
-	print(bestscore)
-	pass
-
-func load_scoreboard():
-	var file = File.new()
-	if not file.file_exists(filepath): return
-	file.open(filepath, File.READ)
-	bestscore = file.get_var()
-	file.close()
-	pass
-
-func save_bestscore():
-	var file = File.new()
-	file.open(filepath, File.WRITE)
-	file.store_var(bestscore)
-	file.close()
-	pass
-	
-func set_bestscore(new_value):
-	bestscore = new_value
-	save_bestscore()
 	pass
