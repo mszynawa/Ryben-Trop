7ddd514fe51ffa031bdbf429082127a7674dc7ec
mszynawa <m.szynawa@yahoo.com>
2019-03-09 00:26:21 +0100

    save commit

--- Scripts/GUI/MainMenu.gd
+++ Scripts/GUI/MainMenu.gd
@@ -8,6 +8,7 @@ func _ready():
 
 func _on_TextureButton_gui_input(event):
 	if event is InputEventScreenTouch and event.pressed:
+		
 #warning-ignore:return_value_discarded
 		get_tree().change_scene("res://Scenes/GameRoot.tscn")
 
